a77ca497531084928d97a61ccf2e151f5a902a1f
Grzegorz <rubyonrailscoding@gmail.com>
2016-04-10 20:37:34 +0200

    Created controller, models, migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'regulamin', to: "static#terms", as: :terms
+
+  get 'polityka-prywatnosci', to: "static#privacy", as: :privacy
+
+  get 'dostawa', to: "static#shipping", as: :shipping
+
+  get 'o-sklepie', to: "static#about", as: :about
+
+  root 'products#index'
+
+  resources :products, only: [:show, :index]
+  resources :categories, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
