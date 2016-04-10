629b441edf88eb655e29ca782dedd0804cb087f5
Grzegorz <rubyonrailscoding@gmail.com>
2016-04-11 00:23:49 +0200

    Added admin panel(crud)

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,44 @@
+class Admin::CategoriesController < Admin::BaseController
+def index
+  	@categories = Category.all
+  end
+
+  def new
+  	@category = Category.new
+  end
+
+  def create
+  	@category = Category.new(product_params)
+  	if @category.save
+  		redirect_to admin_categories_path, notice: "Pomyślnie dodano kategorię."
+  	else
+  		render action: :new
+  	end
+  end
+
+    def edit
+  	@category = Category.find(params[:id])
+  end
+
+  def update
+  	@category = Category.find(params[:id])
+  	if @category.update_attributes(category_params)
+  	  redirect_to admin_categories_path, notice: "Pomyślnie zmieniono kategorię"
+  	else
+  		render action: :edit
+  	end
+  end
+
+  private
+
+  def product_params
+  	params.require(:product).permit(
+  		:name,
+  		:category_id,
+  		:description,
+  		:long_description,
+  		:price,
+  		:photo
+  	)
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,5 @@ class Category < ActiveRecord::Base
 
 	def to_param
 	 "#{id}-#{name}".parameterize
-		
 	end
 end
